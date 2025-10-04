9db4bb8325faa3f89936e284fbb67899a939918c
rafasmour <rafasmour@gmail.com>
2025-10-04 15:12:28 +0300

    made vite hot reload work

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -13,6 +13,10 @@ export default function Welcome() {
                     href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
                     rel="stylesheet"
                 />
+                <meta
+                    http-equiv="Content-Security-Policy"
+                    content="upgrade-insecure-requests"
+                />
             </Head>
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
                 <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,30 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import viteBasicSslPlugin from '@vitejs/plugin-basic-ssl';
+import * as os from 'node:os';
+function getIPv4(): string | undefined {
+    const ifaces = os.networkInterfaces();
+    for (const entries of Object.values(ifaces)) {
+        for (const i of entries ?? []) {
+            if (i.family === 'IPv4' && !i.internal) return i.address;
+        }
+    }
+    return undefined;
+}
 
+const viteIP =  getIPv4() || '127.0.0.1';
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-            detectTls: `${process.env.APP_URL}`
+            refresh: false,
+        }),
+        viteBasicSslPlugin({
+            certDir: "./certs",
+            domains: [process.env.APP_URL ?? "localhost"],
+            name: "laravel-react-starter-kit",
         }),
         react(),
         tailwindcss(),
@@ -18,6 +34,11 @@ export default defineConfig({
             formVariants: true,
         }),
     ],
+    server: {
+        cors: true,
+        host: viteIP,
+        hmr: true
+    },
     esbuild: {
         jsx: 'automatic',
     },
