4c102c8709aed8d19878c33c77c2a697f6521881
rafasmour <rafasmour@gmail.com>
2025-10-11 15:46:12 +0300

    added shouldbroadcastnow implementation on all events

--- app/Events/StopGame.php
+++ app/Events/StopGame.php
@@ -5,10 +5,11 @@ namespace App\Events;
 use App\Models\Room;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class StopGame
+class StopGame implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
